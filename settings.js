71d3754bf10ab027eeb1aa258d8576a9e208c018
Jennifer Fong <jen@ednapiranha.com>
2013-07-24 16:12:38 -0400

    updated maxage

--- settings.js
+++ settings.js
@@ -2,6 +2,7 @@
 module.exports = function(app, configurations, express) {
   var clientSessions = require('client-sessions');
   var nconf = require('nconf');
+  var maxAge = 24 * 60 * 60 * 1000 * 28;
 
   nconf.argv().env().file({ file: 'local.json' });
 
@@ -24,7 +25,11 @@ module.exports = function(app, configurations, express) {
       // will expire after duration (ms)
       // from last session.reset() or
       // initial cookieing.
-      duration: 24 * 60 * 60 * 1000 * 28 // 4 weeks
+      duration: maxAge, // 4 weeks
+      cookie: {
+        httpOnly: true,
+        maxAge: maxAge
+      }
     }));
     app.use(function(req, res, next) {
       res.locals.session = req.session;
