34ab573fbe69f84a55cb803cf94b9fbe4b3f4537
Jennifer Fong <jen@ednapiranha.com>
2013-07-25 14:58:08 -0400

    added chat functionality and fixed quirk with user context

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,61 +6,95 @@ define(['jquery'],
   var body = $('body');
   var friendList = $('.friends ul');
   var chatList = $('.chats ul');
+  var chatUser = $('#friend');
 
-  body.on('click', '#login', function (ev) {
-    ev.preventDefault();
+  var renderFriend = function (f) {
+    setTimeout(function () {
+      var li = $('<li data-action="chat-friend" data-user="' +
+        f.user + '"><img src="' + f.avatar + '" data-action="chat-friend" ' +
+        'data-user="' + f.user + '"><li>');
+      friendList.append(li);
+   }, 1);
+  };
 
-    navigator.id.get(function (assertion) {
-      if (!assertion) {
-        return;
-      }
-
-      $.ajax({
-        url: '/persona/verify',
-        type: 'POST',
-        data: { assertion: assertion },
-        dataType: 'json',
-        cache: false
-      }).done(function (data) {
-        if (data.status === 'okay') {
-          document.location.href = '/login';
-        } else {
-          console.log('Login failed because ' + data.reason);
-        }
-      });
-    });
-  });
-
-  var renderFriend = function (avatar) {
-    var li = $('<li><img src="' + avatar + '"><li>');
-    friendList.append(li);
+  var renderChat = function (c) {
+    setTimeout(function () {
+      var li = $('<li data-action="chat-message" data-user="' +
+        c.user + '"><p>' + c.chat + '</p><li>');
+      chatList.append(li);
+    }, 1);
   };
 
   if (!!body.data('authenticated')) {
     $.get('/get/friends', function (data) {
       for (var i = 0; i < data.friends.length; i ++) {
-        setTimeout(function () {
-          renderFriend(data.friends[i - 1].avatar);
-        }, 1);
+        renderFriend(data.friends[i]);
       }
     });
   }
 
-  body.on('click', '#logout', function(ev) {
-    ev.preventDefault();
+  body.on('click', function (ev) {
+    var self = $(ev.target);
 
-    $.ajax({
-      url: '/persona/logout',
-      type: 'POST',
-      dataType: 'json',
-      cache: false
-    }).done(function (data) {
-      if (data.status === 'okay') {
-        document.location.href = '/';
-      } else {
-        console.log('Logout failed because ' + data.reason);
-      }
-    });
+    switch (self.data('action')) {
+      case 'login':
+        ev.preventDefault();
+        navigator.id.get(function (assertion) {
+          if (!assertion) {
+            return;
+          }
+
+          $.ajax({
+            url: '/persona/verify',
+            type: 'POST',
+            data: { assertion: assertion },
+            dataType: 'json',
+            cache: false
+          }).done(function (data) {
+            if (data.status === 'okay') {
+              document.location.href = '/login';
+            } else {
+              console.log('Login failed because ' + data.reason);
+            }
+          });
+        });
+        break;
+
+      case 'logout':
+        ev.preventDefault();
+        $.ajax({
+          url: '/persona/logout',
+          type: 'POST',
+          dataType: 'json',
+          cache: false
+        }).done(function (data) {
+          if (data.status === 'okay') {
+            document.location.href = '/logout';
+          } else {
+            console.log('Logout failed because ' + data.reason);
+          }
+        });
+        break;
+
+      case 'chat-friend':
+        ev.preventDefault();
+        friendList.find('li').removeClass('on');
+        body.find('#add-chat').removeClass('hidden');
+        self.closest('li').addClass('on');
+        chatUser.val(self.data('user'));
+        chatList.empty();
+
+        $.get('/get/chats/' + self.data('user'), function (data) {
+          for (var i = 0; i < data.chats.chats.length; i ++) {
+            var chat = {
+              user: data.chats.chats[i].key.split('!')[1],
+              chat: data.chats.chats[i].value
+            };
+            renderChat(chat);
+          }
+        });
+        break;
+    }
   });
 
   body.on('submit', function (ev) {
@@ -70,7 +104,16 @@ define(['jquery'],
       case 'add-friend':
         ev.preventDefault();
         $.post(self.attr('action'), self.serialize(), function (data) {
-          renderFriend(data.friend.avatar);
+          renderFriend(data.friend);
+          self.find('#add-friend').val('');
+        });
+        break;
+
+      case 'add-chat':
+        ev.preventDefault();
+        $.post(self.attr('action'), self.serialize(), function (data) {
+          renderChat(data);
+          self.find('#add-chat', '#friend').val('');
         });
         break;
     }
--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,51 @@
 module.exports = function (app, isLoggedIn) {
   var gravatar = require('gravatar');
   var Parallax = require('meatspace-parallax');
-  var parallax = new Parallax({
+  var parallax = new Parallax('none', {
     db: '../db',
-    limit: 100
+    limit: 20
   });
 
-  var parallax;
-
   app.get('/', function (req, res) {
-    res.render('index');
+    if (req.session.email) {
+      res.redirect('/dashboard');
+    } else {
+      res.render('index');
+    }
   });
 
   app.get('/login', isLoggedIn, function (req, res) {
-    parallax = new Parallax(req.session.email, {
-      db: '../db',
-      limit: 20
-    });
-
     res.redirect('/dashboard');
   });
 
+  app.get('/logout', isLoggedIn, function (req, res) {
+
+    res.redirect('/');
+  });
+
   app.get('/dashboard', isLoggedIn, function (req, res) {
+    parallax.user = req.session.email;
+    parallax.friendsLevel = parallax.db.sublevel(parallax.user + '!friends');
+    parallax.friendList = parallax.db.sublevel(parallax.user + '!friendlist');
     res.render('dashboard');
   });
 
   app.get('/get/friends', isLoggedIn, function (req, res) {
+    var friends = [];
+
+    var addFriend = function (f, i) {
+      var user = f.friends[i].key;
+
+      friends.push({
+        user: user,
+        avatar: gravatar.url(user, { s: 100 })
+      })
+
+      if (friends.length === f.friends.length) {
+        res.json({ friends: friends });
+      }
+    };
+
     parallax.getFriends(function (err, f) {
       if (err) {
         res.status(400);
@@ -36,20 +56,37 @@ module.exports = function (app, isLoggedIn) {
         var friends = [];
 
         for (var i = 0; i < f.friends.length; i ++) {
-          friends.push({
-            avatar: gravatar.url(f.friends[i].key, { s: 100 })
-          })
-
-          if (friends.length === f.friends.length) {
-            res.json({ friends: friends });
-          }
+          addFriend(f, i);
         }
       }
     });
   });
 
-  app.post('/add/chat', isLoggedIn, function (req, res) {
+  app.get('/get/chats/:user', isLoggedIn, function (req, res) {
+    parallax.getChats(req.params.user, false, false, function (err, c) {
+      if (err) {
+        res.status(400);
+        res.json({ error: err.toString() });
+      } else {
+        res.json({
+          chats: c
+        });
+      }
+    });
+  });
 
+  app.post('/add/chat', isLoggedIn, function (req, res) {
+    parallax.addChat(req.body.friend, req.body.chat, function (err, c) {
+      if (err) {
+        res.status(400);
+        res.json({ error: err.toString() });
+      } else {
+        res.json({
+          user: req.body.friend,
+          chat: c
+        });
+      }
+    });
   });
 
   app.post('/add/friend', isLoggedIn, function (req, res) {
