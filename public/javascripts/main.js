f5c4a368ff7c2c37d9a1c98643e03a6c00308969
Jennifer Fong <jen@ednapiranha.com>
2013-07-25 10:23:50 -0400

    added friend list/ gravatar support

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,6 +4,8 @@ define(['jquery'],
   'use strict';
 
   var body = $('body');
+  var friendList = $('.friends ul');
+  var chatList = $('.chats ul');
 
   body.on('click', '#login', function (ev) {
     ev.preventDefault();
@@ -29,6 +31,21 @@ define(['jquery'],
     });
   });
 
+  var renderFriend = function (avatar) {
+    var li = $('<li><img src="' + avatar + '"><li>');
+    friendList.append(li);
+  };
+
+  if (!!body.data('authenticated')) {
+    $.get('/get/friends', function (data) {
+      for (var i = 0; i < data.friends.length; i ++) {
+        setTimeout(function () {
+          renderFriend(data.friends[i - 1].avatar);
+        }, 1);
+      }
+    });
+  }
+
   body.on('click', '#logout', function(ev) {
     ev.preventDefault();
 
@@ -53,7 +70,7 @@ define(['jquery'],
       case 'add-friend':
         ev.preventDefault();
         $.post(self.attr('action'), self.serialize(), function (data) {
-          console.log(data);
+          renderFriend(data.friend.avatar);
         });
         break;
     }
