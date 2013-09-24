f69b4d0f7844bb3d97468a1773c7ae3b9d125dbd
Jennifer Fong <jen@ednapiranha.com>
2013-09-24 09:36:13 -0700

    remove login

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = function (app, io, isLoggedIn) {
-  var gravatar = require('gravatar');
   var Publico = require('meatspace-publico');
   var publico = new Publico('none', {
     db: './db',
@@ -10,19 +9,6 @@ module.exports = function (app, io, isLoggedIn) {
 
   app.get('/', function (req, res) {
     res.render('index');
-  });
-
-  app.get('/login', isLoggedIn, function (req, res) {
-    res.render('index');
-  });
-
-  app.get('/logout', isLoggedIn, function (req, res) {
-    res.redirect('/');
-  });
-
-  app.get('/dashboard', isLoggedIn, function (req, res) {
-    publico.user = req.session.email;
-    res.render('dashboard');
   });
 
   app.get('/get/chats', function (req, res) {
@@ -36,10 +22,10 @@ module.exports = function (app, io, isLoggedIn) {
     });
   });
 
-  app.post('/add/chat', isLoggedIn, function (req, res) {
+  app.post('/add/chat', function (req, res) {
     publico.addChat(req.body.message, {
       ttl: 600000,
-      media: req.body.picture || gravatar.url(req.session.email, { s: 300 })
+      media: req.body.picture || '/images/avatar.jpg'
     }, function (err, c) {
       if (err) {
         res.status(400);
