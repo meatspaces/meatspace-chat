a8d8dfd72c27b6ccd04a5361b491d2760ffc6c10
Cesar Carruitero <cesar@mozilla.pe>
2013-10-02 10:55:24 -0500

    fix videoShooter call

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,4 @@
-define(['jquery', './base/gumhelper', './base/videoshooter'],
+define(['jquery', './base/gumhelper', './base/videoShooter'],
   function($, gumHelper, VideoShooter) {
   'use strict';
 
