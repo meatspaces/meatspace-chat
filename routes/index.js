48aa246093474f337efa8827d87938ea0f9dfbd1
Jennifer Fong <jen@ednapiranha.com>
2013-10-29 06:22:42 -0700

    updated license info; refactored post

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ module.exports = function (app, io) {
   var crypto = require('crypto');
   var Publico = require('meatspace-publico');
   var nativeClients = require('../clients.json');
+
   var publico = new Publico('none', {
     db: './db',
     limit: 20
@@ -30,38 +31,49 @@ module.exports = function (app, io) {
     });
   });
 
+  var addChat = function (message, picture, fingerprint, next) {
+    publico.addChat(message.slice(0, 150), {
+      ttl: 600000,
+      media: picture,
+      fingerprint: fingerprint
+    }, function (err, c) {
+      if (err) {
+        next(err);
+      } else {
+        try {
+          io.sockets.emit('message', {
+            chat: {
+              key: c.key,
+              value: {
+                fingerprint: fingerprint,
+                created: c.created,
+                media: c.media,
+                ttl: c.ttl,
+                message: c.message
+              }
+            }
+          });
+
+          next(null, 'sent!');
+        } catch (err) {
+          next(new Error('Could not emit message'));
+        }
+      }
+    });
+  };
+
   app.post('/add/chat', function (req, res, next) {
     if (req.body.picture) {
       var userId = crypto.createHash('md5').update(req.body.fingerprint + req.connection.remoteAddress).digest('hex');
 
-      if ((userId === req.body.userid && !req.body.apiKey) || nativeClients.indexOf(req.body.apiKey) > -1) {
-        publico.addChat(req.body.message.slice(0, 150), {
-          ttl: 600000,
-          media: req.body.picture,
-          fingerprint: userId
-        }, function (err, c) {
+      if (userId === req.body.userid) {
+
+        addChat(req.body.message, req.body.picture, userId, function (err, status) {
           if (err) {
             res.status(400);
             res.json({ error: err.toString() });
           } else {
-            try {
-              io.sockets.emit('message', {
-                chat: {
-                  key: c.key,
-                  value: {
-                    fingerprint: userId,
-                    created: c.created,
-                    media: c.media,
-                    ttl: c.ttl,
-                    message: c.message
-                  }
-                }
-              });
-            } catch (err) {
-              console.log('Could not emit message');
-            }
-
-            res.json({ status: 'sent!' });
+            res.json({ status: status });
           }
         });
       } else {
@@ -74,42 +86,17 @@ module.exports = function (app, io) {
     }
   });
 
-  var nativeClients = require('../clients.json');
-
   io.sockets.on('connection', function (socket) {
     socket.on('message', function (data) {
       if (nativeClients.indexOf(data.apiKey) > -1) {
-        publico.addChat(data.message.slice(0, 150), {
-          ttl: 600000,
-          media: data.picture,
-          fingerprint: data.fingerprint
-        }, function (err, c) {
-          if (err) {
-            console.log(err.toString());
-          } else {
-            try {
-              io.sockets.emit('message', {
-                chat: {
-                  key: c.key,
-                  value: {
-                    fingerprint: data.fingerprint,
-                    created: c.created,
-                    media: c.media,
-                    ttl: c.ttl,
-                    message: c.message
-                  }
-                }
-              });
-            } catch (err) {
-              console.log(err.toString());
-              console.log('Could not emit message');
-            }
 
+        addChat(data.message, data.picture, data.fingerprint, function (err) {
+          if (err) {
+            console.log('error posting ', err.toString());
           }
         });
-
       } else {
-        console.log('Invalid apiKey: ' + data.apiKey);
+        console.log('Invalid apiKey: ', data.apiKey);
       }
     });
   });
