1c1776ae09fcf6f9ecb432964a04035298f2c224
Jennifer Fong <jen@ednapiranha.com>
2013-11-06 08:00:32 -0800

    fallback to long polling

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ nconf.argv().env().file({ file: 'local.json' });
 var io = require('socket.io').listen(server);
 
 io.configure(function () {
-  io.set('transports', ['websocket']);
+  io.set('transports', ['websocket', 'xhr-polling']);
   io.set('polling duration', 10);
   io.set('log level', 1);
 });
