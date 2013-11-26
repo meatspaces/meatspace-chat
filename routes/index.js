f3318d825979d5f9006f6136fd30019d4bfd22b4
Jennifer Fong <jen@ednapiranha.com>
2013-11-26 08:43:23 -0800

    ip fix using x-forwarded-for

--- routes/index.js
+++ routes/index.js
@@ -65,7 +65,7 @@ module.exports = function (app, nconf, io) {
   app.get('/', function (req, res) {
     var currDate = Date.now();
     logger.put('landing-page!' + currDate, {
-      ip: req.ip,
+      ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
       created: currDate
     });
     res.render('index');
@@ -79,7 +79,7 @@ module.exports = function (app, nconf, io) {
 
   app.get('/ip', function (req, res) {
     res.json({
-      ip: req.ip
+      ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress
     });
   });
 
@@ -115,18 +115,19 @@ module.exports = function (app, nconf, io) {
   };
 
   app.post('/add/chat', function (req, res, next) {
-    var userId = crypto.createHash('md5').update(req.body.fingerprint + req.ip).digest('hex');
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    var userId = crypto.createHash('md5').update(req.body.fingerprint + ip).digest('hex');
 
     if (req.body.picture) {
       if (userId && userId === req.body.userid) {
-        addChat(req.body.message, req.body.picture, req.body.fingerprint, userId, req.ip, function (err, status) {
+        addChat(req.body.message, req.body.picture, req.body.fingerprint, userId, ip, function (err, status) {
           if (err) {
             res.status(400);
             res.json({ error: err.toString() });
           } else {
             var currDate = Date.now();
             logger.put('web!' + currDate, {
-              ip: req.ip,
+              ip: ip,
               fingerprint: userId,
               created: currDate
             });
