6f680c9edeb9bac32f2d9e1e87d9fc66939e6582
taotetek <taotetek@gmail.com>
2014-01-13 01:26:51 +0000

    multiple stats endpoints + improved stats msg

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ module.exports = function (app, nconf, io, zio, topic_in, topic_out) {
   };
 
   var emitChat = function (socket, chat, zio, topic_out) {
-    var statmsg = topic_out.concat(JSON.stringify({ fingerprint: chat.value.fingerprint }));
+    var statmsg = topic_out.concat('|', JSON.stringify({ epoch_ms: (new Date).getTime(), fingerprint: chat.value.fingerprint }));
     zio.send(statmsg);
     socket.emit('message', { chat: chat });
   };
@@ -64,8 +64,7 @@ module.exports = function (app, nconf, io, zio, topic_in, topic_out) {
         next(err);
       } else {
         try {
-          var payload = JSON.stringify({ fingerprint: fingerprint });
-          var statmsg = topic_in.concat(payload);
+          var statmsg = topic_in.concat('|', JSON.stringify({ epoch_ms: (new Date).getTime(), fingerprint: fingerprint }));
           zio.send(statmsg);
           emitChat(io.sockets, { key: c.key, value: c }, zio, topic_out);
           next(null, 'sent!');
