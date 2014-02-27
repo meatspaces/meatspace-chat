eb59aedc0eaf0f6c41e16e641fb9c2ad26be4feb
Travis Collins <travis@tec27.com>
2014-02-27 02:53:18 -0800

    Letterbox images when they don't match aspect ratio.
    We probably also need to handle orientation changes and flip the
    width/height in those cases. I *believe* mobile browsers handle that
    consistently. This should work better for the initial orientation,
    although the black bars can be rather large. Ideally I think we could
    try to get the browser to get us a resolution closer in aspect ratio
    to what we want, although I haven't had much success with constraints
    as of yet.
    
    We could also consider doing some sort of cropping and/or zooming, we
    just need to make sure the preview matches up with what we're doing
    behind the scenes.

--- public/javascripts/base/videoShooter.js
+++ public/javascripts/base/videoShooter.js
@@ -1,21 +1,54 @@
 define(['Animated_GIF'], function (Animated_GIF) {
   'use strict';
 
-  function VideoShooter (videoElement) {
+  function VideoShooter (videoElement, desiredWidth, desiredHeight) {
     this.getShot = function (callback, numFrames, interval, progressCallback) {
       numFrames = numFrames !== undefined ? numFrames : 3;
       interval = interval !== undefined ? interval : 0.1; // In seconds
+
+      var canvas = document.createElement('canvas');
+      canvas.width = desiredWidth;
+      canvas.height = desiredHeight;
+      var context = canvas.getContext('2d');
+      context.fillStyle = 'rgb(0,0,0)';
 
       var pendingFrames = numFrames;
       var ag = new Animated_GIF({
         workerPath: 'javascripts/lib/animated-gif/dist/Animated_GIF.worker.min.js'
       });
-      ag.setSize(videoElement.width, videoElement.height);
+      ag.setSize(desiredWidth, desiredHeight);
       ag.setDelay(interval);
 
+      var desiredRatio = desiredWidth / desiredHeight;
+      var videoRatio = videoElement.width / videoElement.height;
+      var isLetterboxed = false;
+      var scaledWidth;
+      var scaledHeight;
+      if (desiredWidth === videoElement.width && desiredHeight === videoElement.height) {
+        scaledWidth = desiredWidth;
+        scaledHeight = desiredHeight;
+      } else if (Math.abs(videoRatio - desiredRatio) < 0.1) {
+        scaledWidth = desiredWidth;
+        scaledHeight = desiredHeight;
+      } else {
+        isLetterboxed = true;
+        var scaleFactor = videoElement.width > videoElement.height ?
+          (desiredWidth / videoElement.width) : (desiredHeight / videoElement.height);
+        scaledWidth = Math.round(videoElement.width * scaleFactor);
+        scaledHeight = Math.round(videoElement.height * scaleFactor);
+      }
 
       var captureFrame = function() {
-        ag.addFrame(videoElement);
+        if (isLetterboxed) {
+          context.fillRect(0, 0, desiredWidth, desiredHeight);
+        }
+
+        context.drawImage(videoElement,
+          Math.abs(desiredWidth - videoElement.width) / 2,
+          Math.abs(desiredHeight - videoElement.height) / 2,
+          scaledWidth, scaledHeight);
+
+        ag.addFrameImageData(context.getImageData(0, 0, desiredWidth, desiredHeight));
         pendingFrames--;
 
         // Call back with an r value indicating how far along we are in capture
@@ -36,6 +69,19 @@ define(['Animated_GIF'], function (Animated_GIF) {
       captureFrame();
     };
   }
+
+  VideoShooter.getDimensions = function (actualWidth, actualHeight, desiredWidth, desiredHeight) {
+    var result = { width: desiredWidth, height: desiredHeight };
+    if (actualWidth > actualHeight) {
+      result.height =
+        Math.min(desiredHeight, Math.round(desiredWidth * actualHeight / actualWidth));
+    } else {
+      result.width =
+        Math.min(desiredWidth, Math.round(desiredHeight * actualWidth / actualHeight));
+    }
+
+    return result;
+  };
 
   return VideoShooter;
 });
