aaa322e9a3a86a4a8251fea4e1459db5d65fb06d
Travis Collins <travis@tec27.com>
2014-02-27 00:15:24 -0800

    Remove unnecessary/unused canvas element.

--- public/javascripts/base/videoShooter.js
+++ public/javascripts/base/videoShooter.js
@@ -2,20 +2,13 @@ define(['Animated_GIF'], function (Animated_GIF) {
   'use strict';
 
   function VideoShooter (videoElement) {
-    var canvas = document.createElement('canvas');
-    var context = canvas.getContext('2d');
-    context.scale(-1, 1); // mirror flip preview back to the normal direction
-
-    canvas.width = videoElement.width;
-    canvas.height = videoElement.height;
-
     this.getShot = function (callback, numFrames, interval, progressCallback) {
       numFrames = numFrames !== undefined ? numFrames : 3;
       interval = interval !== undefined ? interval : 0.1; // In seconds
 
       var pendingFrames = numFrames;
       var ag = new Animated_GIF({ workerPath: 'javascripts/lib/animated-gif/dist/Animated_GIF.worker.min.js' });
-      ag.setSize(canvas.width, canvas.height);
+      ag.setSize(videoElement.width, videoElement.height);
       ag.setDelay(interval);
 
       captureFrame();
